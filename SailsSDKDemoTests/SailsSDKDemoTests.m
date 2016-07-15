ab27d30a22e0a73af4206894d6da3f9bd75e09e8
Richard Chin <richjing@gmail.com>
2016-07-15 16:37:56 +0800

    no message

---
+++ SailsSDKDemoTests/SailsSDKDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  SailsSDKDemoTests.m
+//  SailsSDKDemoTests
+//
+//  Created by Robert.Hsueh on 2014/7/7.
+//  Copyright (c) 2014年 Robert.Hsueh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SailsSDKDemoTests : XCTestCase
+
+@end
+
+@implementation SailsSDKDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
