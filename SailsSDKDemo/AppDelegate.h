ce7a7cf1ad8de67f5bf9a062e0ded5204eb56177
Richard Chin <richjing@gmail.com>
2016-07-15 16:06:46 +0800

    v1.5.0 first commit to github

---
+++ SailsSDKDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  SailsSDKDemo
+//
+//  Created by Robert.Hsueh on 2014/7/7.
+//  Copyright (c) 2014年 Robert.Hsueh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ SailsSDKDemo/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  SailsSDKDemo
+//
+//  Created by Robert.Hsueh on 2014/7/7.
+//  Copyright (c) 2014年 Robert.Hsueh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
